0c9666a1bd832810c71a822e2702d1bd4ba5b659
heypartner <kevin@heypartner.com>
2017-05-24 08:17:57 -0600

    Update kw-embed.factory.js

--- core/kw-embed.factory.js
+++ core/kw-embed.factory.js
@@ -7,8 +7,12 @@ function FavsEmbed (){
       return embedPodcast(fav);
     } else if (/youtube/.test(fav.url)) {
       return embedVideo(fav);
+    } else if (/ted.com/.test(fav.url)) {
+      return embedTEDVideo(fav);
+    } else if (/amazon/.test(fav.url)) {
+      return embedBook(fav);
     } else {
-      console.log("didn't find anything.")
+      return embedLink(fav);
     }
   }
   function embedPodcast(fav){    
@@ -30,6 +34,23 @@ function FavsEmbed (){
       //error
       console.log('error parsing youtube url');
     }
+  }
+  function embedBook(fav){
+    var result;
+    if(result = fav.url.match("/([a-zA-Z0-9]{10})(?:[/?]|$)")) {
+      fav.embedSrc = 'https://read.amazon.com/kp/card?asin='+ result[0] + '&preview=inline&linkCode=kpe&ref_=cm_sw_r_kb_dp_pX4hzbME3Y9BB';
+      fav.embedType = "podcast";
+    }    
+    return fav;
+  }
+  function embedTEDVideo(fav){
+    fav.embedSrc = "https://embed." + fav.url.slice(12);
+    fav.embedType = "TED";
+    return fav;
+  }
+  function embedLink(fav) {
+    console.log("not supported, yet.");
+    alert("Not supported.  Try locationURLs from Youtube, iheart podcasts, Amazon Books or TED Talks.");
   }
 
   return {
